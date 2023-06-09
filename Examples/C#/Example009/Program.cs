31d64ccd9e312c135576fd898d0d91b4a4bc3e2f
Ilya Bakushkin <dlupus@vk.com>
2023-06-09 21:59:36 +0300

    Examples 9 and 10

--- Examples/C#/Example009/Program.cs
+++ Examples/C#/Example009/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое целое число-");
+string input_1 = Console.ReadLine() ?? "";
+Console.Write("Введите второе целое число-");
+string input_2 = Console.ReadLine() ?? "";
+
+int numIn_1 = int.Parse(input_1);
+int numIn_2 = int.Parse(input_2);
+int result = numIn_1 * numIn_1;
+
+if (result == numIn_2){
+  Console.Write("Yes");
+}else{
+  Console.WriteLine("No");
+}
---
+++ Examples/C#/Example010/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.Write("Введите первое целое число-");
+string input_1 = Console.ReadLine() ?? "";
+int userNum = int.Parse(input_1);
+
+switch (userNum)
+{
+  case 1:
+    Console.WriteLine("Понедельник");
+    break;
+  case 2:
+    Console.WriteLine("Вторник");
+    break;
+  case 3:
+    Console.WriteLine("Среда");
+    break;
+  case 4:
+    Console.WriteLine("Четверг");
+    break;
+  case 5:
+    Console.WriteLine("Пятница");
+    break;
+  case 6:
+    Console.WriteLine("Суббота");
+    break;
+  case 7:
+    Console.WriteLine("Воскресенье");
+    break;
+  default:
+    Console.WriteLine("Такого дня недели нет...");
+    break;
+}
+
