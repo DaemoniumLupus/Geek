12bff829223e7f616363d2201fb656fd3f6b9147
Ilya Bakushkin <dlupus@vk.com>
2023-06-14 09:33:14 +0300

    Add task 13

--- Examples/C#/Homework_2_seminar/Task_13/Program.cs
+++ Examples/C#/Homework_2_seminar/Task_13/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+int Transform(int num)
+{
+  while (num > 999)
+  {
+    num /= 10;
+  }
+  return num;
+}
+int Result(int num){
+
+}
+
+int res;
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+  Console.WriteLine($"{num} -> Третьей цифры нет");
+}
+else if (num > 999)
+{
+  num = Transform(num);
+  res = Result(num);
+} else{
+res = Result(num);
+}
+  
+
+
+
