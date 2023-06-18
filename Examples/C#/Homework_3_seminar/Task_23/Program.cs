989dfca87a1f1ef860a59aef187150ffe3d7e069
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-06-18 10:56:42 +0300

    Add task 23

--- Examples/C#/Homework_3_seminar/Task_23/Program.cs
+++ Examples/C#/Homework_3_seminar/Task_23/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num;
+try
+{
+  Console.Write("Enter number: ");
+  num = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception exc)
+{
+  Console.WriteLine($"Ошибка ввода данных{exc.Message}");
+  return;
+}
+
+Console.Write($"{num} -> 1");
+if (num > 1)
+{
+Console.Write(", ");
+  for (int i = 2; i < num; i++)
+  {
+    Console.Write($"{Math.Pow(i, 3)}, ");
+  }
+Console.Write($"{Math.Pow(num, 3)}");
+}
