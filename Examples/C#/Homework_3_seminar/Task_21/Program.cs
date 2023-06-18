be61554e2daa12c93189741bfe87f3af996c3870
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-06-18 10:45:37 +0300

    Add task 21

--- Examples/C#/Homework_3_seminar/Task_21/Program.cs
+++ Examples/C#/Homework_3_seminar/Task_21/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double fx, fy, fz, sx, sy, sz;
+double res;
+try
+{
+  Console.Write("Введите x первой точки: ");
+  fx = Convert.ToDouble(Console.ReadLine());
+  Console.Write("Введите y первой точки: ");
+  fy = Convert.ToDouble(Console.ReadLine());
+  Console.Write("Введите z первой точки: ");
+  fz = Convert.ToDouble(Console.ReadLine());
+  Console.Write("Введите x второй точки: ");
+  sx = Convert.ToDouble(Console.ReadLine());
+  Console.Write("Введите y второй точки: ");
+  sy = Convert.ToDouble(Console.ReadLine());
+  Console.Write("Введите z второй точки: ");
+  sz = Convert.ToDouble(Console.ReadLine());
+}
+catch (Exception exc)
+{
+  Console.WriteLine($"Ошибка ввода данных{exc.Message}");
+  return;
+}
+
+res = Math.Sqrt(Math.Pow(sx - fx, 2) + Math.Pow(sy - fy, 2) + Math.Pow(sz - fz, 2));
+
+Console.WriteLine($"A{{{fx},{fy},{fz}}}; B{{{sx},{sy},{sz}}} -> {res:f2}");
+
