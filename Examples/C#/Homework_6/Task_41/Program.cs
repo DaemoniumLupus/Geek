345f36978e23861b24a666d4fb3af9fdff8c7b2c
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-06-28 19:59:33 +0300

    Add Homework 6

---
+++ Examples/C#/Homework_6/Task_41/Program.cs
@@ -0,0 +1,62 @@
+﻿int size = GetNumberFromUser("Введите количество чисел: ", "Ошибка ввода!");
+int[] mas = new int[size];
+int res;
+
+WriteMassive(mas);
+
+res = NumbersOfPositive(mas);
+
+WriteRes(mas, res);
+
+
+
+void WriteRes(int[] mas, int res)
+{
+  Console.Write("[");
+  for (int i = 0; i < mas.Length; i++)
+  {
+    if (i + 1 == mas.Length)
+    {
+      Console.Write($"{mas[i]}]");
+    }
+    else
+    {
+      Console.Write($"{mas[i]}, ");
+    }
+  }
+  Console.Write($" -> {res}");
+}
+
+int NumbersOfPositive(int[] mas)
+{
+  int count = 0;
+  for (int i = 0; i < mas.Length; i++)
+  {
+    if (mas[i] > 0){
+      count++;
+    }
+  }
+  return count;
+}
+
+void WriteMassive(int[] mas)
+{
+  for (int i = 0; i < mas.Length; i++)
+  {
+    mas[i] = GetNumberFromUser($"Введите {i + 1}-е число: ", "Ошибка ввода!");
+  }
+}
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+  while (true)
+  {
+    Console.Write(message);
+    if (int.TryParse(Console.ReadLine(), out int userNumber))
+    {
+      return userNumber;
+    }
+    Console.WriteLine(errorMessage);
+  }
+}
---
+++ Examples/C#/Homework_6/Task_43/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+double b1 = GetNumberFromUser("Введите b1: ", "Ошибка ввода!");
+double k1 = GetNumberFromUser("Введите k1: ", "Ошибка ввода!");
+double b2 = GetNumberFromUser("Введите b2: ", "Ошибка ввода!");
+double k2 = GetNumberFromUser("Введите k2: ", "Ошибка ввода!");
+
+double[] res = new double[2];
+Calculation(k1, k2, b1, b2, res);
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({res[0]}; {res[1]})");
+
+
+
+
+void Calculation(double k1, double k2, double b1, double b2, double[] res)
+{
+  res[0] = (b2 - b1) / (k1 - k2);
+  res[1] = k1 * res[0] + b1;
+}
+
+
+
+double GetNumberFromUser(string message, string errorMessage)
+{
+  while (true)
+  {
+    Console.Write(message);
+    if (double.TryParse(Console.ReadLine(), out double userNumber))
+    {
+      return userNumber;
+    }
+    Console.WriteLine(errorMessage);
+  }
+}
