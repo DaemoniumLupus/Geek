77877c19675c195b43fd9438639b9b8699627821
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-07-02 21:48:46 +0300

    Add Task 50 and 52

--- Examples/C#/Homework_7/Task_50/Program.cs
+++ Examples/C#/Homework_7/Task_50/Program.cs
@@ -1,12 +1,49 @@
-﻿Console.WriteLine("");
+﻿int[,] arr = new int[10, 10];
+arr = GetArray(arr);
+Console.Write("Введите индекс ячейки в виде двухзначного числа: ");
 int pos = GetNumberFromUser("Ошибка ввода!");
+int i, j;
+
+i = pos / 10;
+j = pos % 10;
+
+if (i < arr.GetLength(0) || j < arr.GetLength(1))
+{
+  Console.WriteLine($"{pos} -> {arr[i, j]}");
+}
+else
+{
+  Console.WriteLine($"{pos} -> Такого числа в массиве нет!");
+}
+PrintArray(arr);
 
 
+void PrintArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    //Console.WriteLine(string.Join(" ", arr[i]));
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(String.Format("{0,3}", arr[i, j]), " ");
 
+    }
+    Console.Write("\n");
+  }
 
+}
 
-
-
+int[,] GetArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = new Random().Next(0, 100);
+    }
+  }
+  return arr;
+}
 
 int GetNumberFromUser(/* string message,  */string errorMessage)
 {
--- Examples/C#/Homework_7/Task_52/Program.cs
+++ Examples/C#/Homework_7/Task_52/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] arr = new int[10, 10];
+arr = GetArray(arr);
+
+PrintArray(arr);
+
+double[] average = new double[10];
+
+Calculation(arr, average);
+
+PrintRes(average);
+
+
+
+void Calculation(int[,] arr, double[] average)
+{
+  int sizeOfColumn = arr.GetLength(0);
+  for (int j = 0; j < arr.GetLength(1); j++)
+  {
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+      average[j] += arr[i, j];
+    }
+    average[j] /= arr.GetLength(0);
+  }
+}
+
+
+int[,] GetArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = new Random().Next(0, 100);
+    }
+  }
+  return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    //Console.WriteLine(string.Join(" ", arr[i]));
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(String.Format("{0,3}", arr[i, j]), " ");
+
+    }
+    Console.Write("\n");
+  }
+  Console.Write("\n");
+}
+
+void PrintRes(double[] average)
+{
+  Console.WriteLine("Среднее арифметическое каждого столбца: ");
+  for (int i = 0; i < arr.GetLength(1); i++)
+  {
+    Console.Write(String.Format("{0,5:f1}", average[i]), " ");
+  }
+}
