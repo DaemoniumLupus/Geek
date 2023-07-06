d4bb44b75511916e7ef9c86b24de7530a31655a5
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-07-06 09:49:36 +0300

    Complited task 58

--- Examples/C#/Homework_8/Task_58/Program.cs
+++ Examples/C#/Homework_8/Task_58/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int[,] mas1 = new int[4, 4];
+int[,] mas2 = new int[4, 4];
+int[,] multiMas = new int[mas1.GetLength(0), mas2.GetLength(1)];
+
+if (mas1.GetLength(0) == mas2.GetLength(1))
+{
+  mas1 = GetArray(mas1);
+  mas2 = GetArray(mas2);
+
+  multiMas = MultiplicatorMatrix(mas1, mas2, multiMas);
+
+  PrintArray(mas1);
+  PrintArray(mas2);
+  PrintArray(multiMas);
+}
+else
+{
+  Console.WriteLine("Нельзя перемножить матрицы");
+}
+
+
+
+
+int[,] MultiplicatorMatrix(int[,] mas1, int[,] mas2, int[,] multiMas)
+{
+  for (int i = 0; i < multiMas.GetLength(0); i++)
+  {
+    for (int j = 0; j < multiMas.GetLength(1); j++)
+    {
+      multiMas[i, j] = MultiplicateElementMatrix(mas1, mas2, i, j);
+
+    }
+  }
+
+  return multiMas;
+}
+
+int MultiplicateElementMatrix(int[,] mas1, int[,] mas2, int str, int column)
+{
+  int sum = 0;
+  for (int i = 0; i < mas1.GetLength(0); i++)
+  {
+    sum += mas1[str, i] * mas2[i, column];
+  }
+
+
+  return sum;
+}
+
+int[,] GetArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = new Random().Next(1, 6);
+    }
+  }
+  return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(String.Format("{0,3}", arr[i, j]), " ");
+
+    }
+    Console.Write("\n");
+  }
+  Console.Write("\n");
+}
