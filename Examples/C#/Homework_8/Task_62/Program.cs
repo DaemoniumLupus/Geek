de4615b04574fdf36a88d77c88a19bb6409adf2e
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-07-08 11:00:42 +0300

    End task 62

--- Examples/C#/Homework_8/Task_62/Program.cs
+++ Examples/C#/Homework_8/Task_62/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 4;
+int[,] result = new int[n, n];
+
+int pos = 0;
+int count = n;
+int value = -n;
+int sum = -1;
+
+do
+{
+  value = -1 * value / n;
+  for (int i = 0; i < count; i++)
+  {
+    sum += value;
+    result[sum / n, sum % n] = pos++;
+  }
+  value *= n;
+  count--;
+  for (int i = 0; i < count; i++)
+  {
+    sum += value;
+    result[sum / n, sum % n] = pos++;
+  }
+} while (count > 0);
+
+for (int i = 0; i < result.GetLength(0); i++)
+{
+  for (int j = 0; j < result.GetLength(1); j++)
+  {
+    Console.Write(String.Format("{0,3}", result[i, j]), " ");
+  }
+  Console.Write("\n");
+}
