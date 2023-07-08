998ebcb090a3d0dc53eb9ab2fdc010c1e8aa459c
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-07-08 11:24:07 +0300

    Finish task 64

--- Examples/C#/Attestation/Task_64/Program.cs
+++ Examples/C#/Attestation/Task_64/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = GetNumberFromUser("Введите целое число: ", "Ошибка ввода");
+Console.Write($"N = {n} -> \"");
+Recursion(n);
+Console.Write("\"");
+
+void Recursion(int n)
+{
+  if (n == 0)
+  {
+    return;
+  }
+  else
+  {
+    if (n == 1) Console.Write($"{n}"); 
+    else Console.Write($"{n},");
+    Recursion(n - 1);
+  }
+}
+
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+  while (true)
+  {
+    Console.Write(message);
+    if (int.TryParse(Console.ReadLine(), out int userNumber))
+    {
+      return userNumber;
+    }
+    Console.WriteLine(errorMessage);
+  }
+}
