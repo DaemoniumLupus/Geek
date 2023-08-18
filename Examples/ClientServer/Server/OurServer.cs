dc8e37de98fde277f7cb69d2b9c2ad54b1b4ec6b
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-08-18 12:45:09 +0300

    Add Client-Server

---
+++ Examples/ClientServer/Server/OurServer.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+  class OurServer
+  {
+    TcpListener server;
+
+    public OurServer()
+    {
+      server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
+      server.Start();
+
+      LoopClients();
+    }
+
+    void LoopClients()// цикл поиска соединения
+    {
+      while(true)
+      {
+        TcpClient client = server.AcceptTcpClient();
+
+        Thread thread = new Thread(() => HandleClient(client));
+        thread.Start();
+
+        
+      }
+    }
+
+    void HandleClient(TcpClient client)
+    {
+      StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);//для каждого клиента свой поток
+      StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+  
+      while(true)
+      {
+        string message = sReader.ReadLine();
+        Console.WriteLine($"Клиент написал - {message}");
+
+
+        Console.WriteLine("Дайте сообщение серверу!");
+        string answer = Console.ReadLine();
+        sWriter.WriteLine(answer);
+        sWriter.Flush();
+      }
+    }
+  }
+}
---
+++ Examples/ClientServer/Server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Server
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Это наш Сервер");
+      OurServer server = new OurServer();
+      
+    }
+  }
+}
