e5128f09e9669153658ccb930cc4c6df1f0ee54a
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-08-25 16:00:55 +0300

    Add Task hard

--- Examples/Python/Seminar_2/Task_hard.py
+++ Examples/Python/Seminar_2/Task_hard.py
@@ -1,1 +1,38 @@
-num = 
+
+def Number_Int(num: int):
+    count = 0
+    while num > 0:
+        num //= 10
+        count += 1
+    return count
+
+def Number_Float(num: float):
+    count = 0
+    if num < 1:
+        count = 1
+        
+    while num % 1 != 0:
+        num *= 10
+    return Number_Int(num) + count
+
+
+
+num = str(input("Enter number: "))
+
+true_int = True
+
+for i in num:
+    if i == '.' or i == ',':
+        true_int = False
+        break
+    
+if true_int and int(num) == 0:
+    print(1)
+elif true_int:
+    print(Number_Int(int(num)))
+else:
+    print(Number_Float(float(num)))
+    
+
+
+    
