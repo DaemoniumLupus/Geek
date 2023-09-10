77c494febf77071814ec4eea097354eaece68a8f
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-09-10 08:51:27 +0300

    update

--- Examples/Python/Seminar_6/Homework/XO.py/main.py
+++ Examples/Python/Seminar_6/Homework/XO.py/main.py
@@ -5,15 +5,6 @@ game = Game()
 game.game_field =[[' ']*3 ,[' ']*3,[' ']*3]
 game.Paint_Choice()
 
-# game_loop = True
-# while game_loop:
-#   print("Select item: ")
-#   print("1. New Game with bot")
-#   print("2. New Game")
-#   print("3. Exit")
-#   choice = input()
-#   match choice:
-#     case 1:
 
 
 
