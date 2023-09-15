9c724394e535ed3e7cc56249c15bfc28e8595e37
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-09-15 22:46:12 +0300

    Finish Phonebook

--- Examples/Python/Seminar_8/Homework/Main.py
+++ Examples/Python/Seminar_8/Homework/Main.py
@@ -1,126 +1,153 @@
-# from tkinter import *
-# import sqlite3  
-# from tkinter import ttk
+
 
 import json
 
-# sp = {'дядя коля': {'phones': ['8-800-555-35-35', '8-900-536-35-35'], 'city': 'Сыктыфкар', 'status': 'дядя'},
-#        'нюрка': {'phones': ['8-555-549-34-35'], 'city': 'Сыктыфкар', 'status': 'тетя'}}
 
-# conn = sqlite3.connect("phonebook.db")
-# cursor = conn.cursor()
+def Load(sp):
+    with open ('phonebook.json','w')as file:
+        file.write(json.dumps(sp,indent=4))
+        file.close()
 
-# cursor.execute("""CREATE TABLE IF NOT EXISTS names ( 
-#                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-#                 name TEXT NOT NULL,
-#                 city TEXT,
-#                 bithD TEXT);
-# """)
-   
-# cursor.execute("""CREATE TABLE IF NOT EXISTS number (
-#                 id_num INTEGER PRIMARY KEY AUTOINCREMENT,
-#                 name_id INTEGER NOT NULL,
-#                 phone text NOT NULL
-#                 )
-# """)
+def Head():
+    print("{:15}    {:10}  {:10}    {}".format('Name','City','Status','Phones'))
 
-# with open ('phonebook.json','w+')as file:
-#     file.write(json.dumps(sp,indent=4))
-#     file.close()
-def Load() -> dict:
+def Data(person,sp):
+    print ("{:15} -> {:10}  {:10} -> {}".format(person,sp[person]['city'],sp[person]['status'],sp[person]['phones']))
+
+def Head_file() -> str:
+    st = "{:15}    {:10}  {:10}    {}".format('Name','City','Status','Phones')
+    return st
+
+def Data_file(person,sp) -> str:
+    st = "{:15} -> {:10}  {:10} -> {}".format(person,sp[person]['city'],sp[person]['status'],sp[person]['phones'])
+    return st
+
+def Read() -> dict:
     with open("phonebook.json","r") as file:
         sp = json.load(file)
         file.close()
     return sp
 
 def All():
-    sp = Load()
-    print("{:10}    {:10}  {:10}    {}".format('Name','City','Status','Phones'))
+    sp = Read()
+    Head()
     for person in sp:
-        print(f"{person:10} -> {sp[person]['city']:10}  {sp[person]['status']:10} -> {sp[person]['phones']}")
+        Data(person,sp)
                 
 def Add():
-    f = input('Введите имя нового контакта: ')
-    sp = Load()
+    new_name = input('Enter name new contact: ')
+    sp = Read()
     exists = True
     for person in sp:
-        if f == str(person):
-            print('Контакт существует!')
+        if new_name == str(person):
+            ans = input('Contact exists!\nDo you want add number this contact(Y):')
+            while ans == 'y' or ans == 'Y':
+                sp[person]['phones'].append(input("Enter number:"))
+                ans = input("Enter one more?(Y):")
             exists = False
     if exists:
-        sp[f] = dict()
-        sp[f]['city'] = input(f"{f} -> city: ")
-        sp[f]['status'] = input(f"{f} -> status: ")
-        sp[f]['phones'] = list()
-        coll = int(input('Сколько номеров вы хотите ввести: '))
-        while coll > 0:
-            sp[f]['phones'].append(input('Введите первый номер: '))
-            coll -= 1
-        print(f"{'Name':10} -> {'City':10},{'Status':10} -> {'Phones'}")
-        for person in sp:
-            print(f"{person:10} -> {sp[person]['city']:10},{sp[person]['status']:10} -> {sp[person]['phones']}")
+        sp[new_name] = dict()
+        sp[new_name]['city'] = input(f"{new_name} -> city: ")
+        sp[new_name]['status'] = input(f"{new_name} -> status: ")
+        sp[new_name]['phones'] = list()
+        coll = int(input('How many numbers you want input?: '))
+        count = 1
+        while count <= coll:
+            sp[new_name]['phones'].append(input('Enter the number {count}: '))
+            count += 1
+    Load(sp)
+
+
             
 def Search():
+    sp = Read()
+    search_text = input("Search: ")
+    for person in sp:
+        if search_text in str(person):
+            Head()
+            Data(person,sp)
+
+def Remove_contact() -> dict:
+    sp = Read()
+    while True:
+        remove_person = input("Remove(exit -> '-1'): ")
+        if remove_person == '-1':
+            return sp
+        for person in sp:
+            if remove_person in str(person):
+                ans = input("Do you want to remove this contact?(Y):")
+                if ans == 'y' or ans == 'Y':
+                    del sp[person]
+                    print("Contact remove.")
+                    return sp
+                    
+
+def Remove_number() -> dict:
+    sp = Read()
+    while True:
+        remove_person = input("Remove number in contact(exit -> '-1'): ")
+        if remove_person == '-1':
+            return sp
+        for person in sp:
+            if remove_person in str(person):
+                Data(person,sp)
+                count = len(sp[person]['phones'])
+                if count > 1:
+                    print(f"What number you want to remove contact? {[i for i in range(1,count + 1)]}:")
+                    i = int(input())
+                    print(sp[person]['phones'][int(i)-1])
+                    ans = input(f"Do you want to remove this number of contact?(Y):")
+                    if ans == 'y' or ans == 'Y':
+                        sp[person]['phones'].pop(int(i)-1) 
+                        print('Number remove.') 
+                        return sp
+                else:
+                    print(sp[person]['phones'][0])
+                    print(f"Do you want to remove this number of contact?(Y/N):")
+                    ans = input(f"Do you want to remove this number of contact?(Y):")
+                    if ans == 'y' or ans == 'Y':
+                        sp[person]['phones'].pop() 
+                        print('Number remove.') 
+                        return sp
+
+    
+def Import():
+    sp = Read()
+    file = open('phoneimport.txt', 'w')
+    file.write(Head_file())
+    file.write('\n')
+    for person in sp:
+        file.write(Data_file(person,sp))
+        file.write('\n')
+
 
 
 while True:
-    command = input('Введите команду: ')
+    command = input('Enter the command: ')
     if command == '/all':
-        print('Вот текущий список номеров') 
+        print('Contact list: ') 
         All()
     elif command == '/add':
         Add()
-    elif command == "/search":
-        search_text = input("Поиск: ")
-        # Search(sp,search_text)
+    elif command == '/search':
+        Search()
+    elif command == '/remove_contact':
+        sp = Remove_contact()
+        Load(sp)
+    elif command == '/remove_number':
+        sp = Remove_number()
+        Load(sp)
+    elif command == '/import':
+        Import()
+
 
 
 
 
             
-    #     sp.append(f)
-    #     print('Фильм был успешно добавлен в коллекцию!')
-    # elif command == '/help':
-    #     print('Здесь какойто мануал')
+   
 
 
-# class Main():
-#     def __init__(self):
-#         self.window = Tk()
-#         self.window.title("Phonebook")
-#         self.window.geometry("1000x400+300+200")   
-
-#         btnframe = Frame(self.window,height=100,width=100,borderwidth=1,relief=SOLID)
-#         btn_add = Button(btnframe,text="Add Contact")
-#         btn_add.pack(side=LEFT,anchor=NW)
-#         btn_search = Button(btnframe,text="Search")
-#         btn_search.pack(side=LEFT,anchor=N)
-#         btnframe.pack(fill=X)
-
-#         columns = ("name", "phone", "email","BD")
-#         self.tree = ttk.Treeview(columns=columns,show="headings")
-#         self.tree.pack(fill='both',expand=1)
-
-#         self.tree.heading("name",text='Name')
-#         self.tree.heading("phone",text="Phone")
-#         self.tree.heading("email",text= "Email")
-#         self.tree.heading("BD",text="Bith Day")
-
-#         for person in sp:
-#             self.tree.insert("",END,values=person)
-
-
-
-#         self.window.mainloop()     
-#     def Table_print(self):
-#         for person in sp:
-#             self.tree.insert("",END,values=person)
-# def Add(sp: dict):
-#     pass
-
-
-
-# m = Main()
 
 
 
