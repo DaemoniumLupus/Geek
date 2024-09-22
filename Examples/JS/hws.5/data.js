c9161dda1c1d044015079fe7ca4caad97b375f2f
Ilya-Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2024-09-22 15:13:28 +0300

    hws5

---
+++ Examples/JS/hws.5/data.js
@@ -0,0 +1,16 @@
+// const data = fetch("https://randomuser.me/api/");
+
+const data = `{ "results": [{ "gender": "male", "name": { "title": "Mr", "first": "Oliver", "last": "Savela" }, "location": { "street": { "number": 7580, "name": "Hämeenkatu" }, "city": "Vähäkyrö", "state": "Uusimaa", "country": "Finland", "postcode": 88835, "coordinates": { "latitude": "-26.4511", "longitude": "-40.0822" }, "timezone": { "offset": "-8:00", "description": "Pacific Time (US & Canada)" } }, "email": "oliver.savela@example.com", "login": { "uuid": "bd2c3fee-327c-40da-bf9f-0f91fd32b504", "username": "organiczebra132", "password": "weed", "salt": "nkBUa1QU", "md5": "4641326ae48479537e9a7cad950bb047", "sha1": "f8876f5ee436e1c2c78c4b5834eec1f4b061b114", "sha256": "c8784bf3a0ce5b947ed55476d37819b404324f832f752e227cb0d493021e38f4" }, "dob": { "date": "2000-11-29T05:27:16.031Z", "age": 23 }, "registered": { "date": "2014-08-17T17:25:37.938Z", "age": 10 }, "phone": "09-821-630", "cell": "042-246-83-36", "id": { "name": "HETU", "value": "NaNNA045undefined" }, "picture": { "large": "https://randomuser.me/api/portraits/men/8.jpg", "medium": "https://randomuser.me/api/portraits/med/men/8.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/8.jpg" }, "nat": "FI" }], "info": { "seed": "53390f7daa4fdf74", "results": 1, "page": 1, "version": "1.4" } }`
+
+// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
+
+// Задание 2
+// 1. Создать файл index.html.
+// 2. Подключить data.js.
+// 3. Сформировать контент из данных (картинка загловок и параграф).
+// 4. Добавить данный контент в вёрстку.
+// 5. * Добавить стили при необходимости (по желанию).
